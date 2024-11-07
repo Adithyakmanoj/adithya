a6b2f3eb8f9002cdd738c5b7bc6d6963c2dabd68
Adithya km <adithyakmanoj@gmail.com>
2024-11-07 15:36:31 +0530

    changes2

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -12,7 +12,7 @@ function Home() {
           <p>I'm a Python full-stack developer skilled in creating responsive web applications with a focus on seamless front-end experiences and robust back-end solutions using frameworks like Django and Flask. I prioritize clean, scalable code and user-centered designs to deliver impactful, high-performance applications.</p>
           <div className='home-action'>
           <div className='home-connect'>
-          <a href="adithyakmanoj@gmail.com" className='link-connect'>Connect with me</a></div>
+          <a href="mailto:adithyakmanoj@gmail.com" className='link-conect'>Connect with me</a></div>
           <div className='resume'>
               <a href={resumeFile} download="Adithya_Resume.pdf" className='link-connect'>My Resume</a> 
             </div>        
